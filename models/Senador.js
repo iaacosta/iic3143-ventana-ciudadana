56dfda76318288d0d3b1f3703cef0491b7f17948
Nacho <iaacosta@uc.cl>
2019-04-28 13:28:23 -0400

    changed schema

--- models/Senador.js
+++ models/Senador.js
@@ -1,33 +1,26 @@
 module.exports = (sequelize, type) => (
   sequelize.define('senadores', {
-    id: {
+    sid: {
       type: type.INTEGER,
       primaryKey: true,
     },
     nombre: {
-      type: type.STRING,
+      type: type.STRING(50),
       allowNull: false,
     },
-    partido_politico: {
-      type: type.STRING,
+    apellido_paterno: {
+      type: type.STRING(50),
       allowNull: false,
     },
-    email: {
-      type: type.STRING,
+    apellido_materno: {
+      type: type.STRING(50),
       allowNull: false,
     },
-    circunscripcion: {
-      type: type.INTEGER,
-      allowNull: false,
-    },
-    region: {
-      type: type.STRING,
-      allowNull: false,
-    },
-    url_foto: {
-      type: type.STRING,
-      allowNull: false,
-    },
-    telefono: type.STRING,
+    partido_politico: type.STRING(50),
+    email: type.STRING(50),
+    telefono: type.STRING(20),
+    curriculum_url: type.STRING(255),
+    foto_url: type.STRING(255),
+    twitter_url: type.STRING(255),
   })
 )
