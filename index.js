2a1ea181bff97ebb48d4ca243fdcab640a1b3e69
Nacho <iaacosta@uc.cl>
2019-04-28 23:19:18 -0400

    changed port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const app = require('./src/app');
 const db = require('./src/models');
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 db.sequelize
   .authenticate()
