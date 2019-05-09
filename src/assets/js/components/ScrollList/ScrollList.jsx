0b614041eef23b85583db21d1f9084ac290a0378
Ignacio Acosta <iaacosta@uc.cl>
2019-05-09 11:16:29 -0400

    agregado fix de las cifras y mensaje cuando esta vacio

--- src/assets/js/components/ScrollList/ScrollList.jsx
+++ src/assets/js/components/ScrollList/ScrollList.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const ScrollList = ({ items, mapFn, height }) => (
-  <ul className="scroll-list" style={{ height: height || '20rem' }}>
-    {items.map(mapFn)}
-  </ul>
-);
+const ScrollList = ({ items, mapFn, height }) => {
+  const isEmpty = items.length === 0;
+  const placeholder = (
+    <p className="scroll-list__no-items">No hay registros disponibles</p>
+  );
+  return (
+    <ul
+      className="scroll-list"
+      style={{ height: (isEmpty && '8rem') || height || '20rem' }}
+    >
+      {!isEmpty ? items.map(mapFn) : placeholder}
+    </ul>
+  );
+};
 
 export default ScrollList;
