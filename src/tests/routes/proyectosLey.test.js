7e3df67edbc31a6f1cdbf65e01cf3111d5ed2ed2
Nacho <iaacosta@uc.cl>
2019-05-08 23:48:39 -0400

    testeo de rutas completo

--- src/tests/routes/proyectosLey.test.js
+++ src/tests/routes/proyectosLey.test.js
@@ -15,9 +15,34 @@ describe('proyectos de ley / ruta', () => {
     await migration.cleanUp();
   });
 
-  test('debe devolver index', async () => {
+  test('"/proyectos-ley" debe responder la con status 200', async () => {
     const response = await request(server).get('/proyectos-ley');
+    expect(response.status).toBe(200);
+  });
+
+  test('"/proyectos-ley/cifras" debe responder con las cifras', async () => {
+    const response = await request(server).get('/proyectos-ley/cifras');
 
     expect(response.status).toBe(200);
+    expect(response.type).toBe('application/json');
+    expect(response.body.cifras.aprobado).toBe(1);
+    expect(response.body.cifras.rechazado).toBe(1);
+    expect(response.body.cifras.suspendido).toBe(1);
+    expect(response.body.cifras.tramitacion).toBe(1);
+  });
+
+  test('"/proyectos-ley/proyectos?tipo=..." debe responder con los proyectos correctos', () => {
+    const estados = ['aprobado', 'rechazado', 'suspendido', 'tramitacion'];
+
+    estados.forEach(async (estado, index) => {
+      const response = await request(server).get(
+        `/proyectos-ley/proyectos?estado=${estado}`,
+      );
+      expect(response.status).toBe(200);
+      expect(response.type).toBe('application/json');
+      expect(response.body.proyectos[0].boletin).toBe(proyectos[index].boletin);
+      expect(response.body.proyectos[0].estado).toBe(proyectos[index].estado);
+      expect(response.body.proyectos[0].resumen).toBe(proyectos[index].resumen);
+    });
   });
 });
