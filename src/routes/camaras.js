a27b38ecfa7987e1633763809b747809bbca77a7
Sofia Schwarzenberg <sschwarzenberg@uc.cl>
2019-05-11 12:50:11 -0400

    senators in pictograph

---
+++ src/routes/camaras.js
@@ -0,0 +1,29 @@
+const KoaRouter = require('koa-router');
+const pkg = require('../../package.json');
+const { Senador, Sequelize, Periodo } = require('../models');
+const Op = Sequelize.Op;
+
+const router = new KoaRouter();
+
+router.get('camaras', '/', async ctx => {
+  const partidos = await Senador.findAll({
+    attributes: ['partido_politico','id'],
+  });
+
+  const years_congress = await Periodo.findAll({
+    attributes: ['sid'],
+    where: {
+      final: {[Op.gte]:2019},
+      cargo :"senador"
+    }
+  });
+
+  await ctx.render('camaras', {
+    partidos: JSON.stringify(partidos),
+    years_congress:JSON.stringify(years_congress)
+  });
+});
+
+
+
+module.exports = router;
