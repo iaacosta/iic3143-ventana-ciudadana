9176bf5c60340f2014240e53a47c2e7d8e4ea883
Nacho <iaacosta@uc.cl>
2019-05-12 11:56:37 -0400

    ruta agregada

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,22 @@
 const KoaRouter = require('koa-router');
-const pkg = require('../../package.json');
+const dayjs = require('dayjs');
 
 const router = new KoaRouter();
 
-router.get('/', async (ctx) => {
-  await ctx.render('index', { appVersion: pkg.version });
+router.get('/', async ctx => {
+  const lastUpdate = await ctx.orm.Update.max('createdAt');
+  let diff = Math.abs(dayjs(lastUpdate).diff(dayjs(), 'hour'));
+  let suffix = diff > 1 ? 'horas' : 'hora';
+
+  if (diff > 24) {
+    diff = Math.abs(dayjs(lastUpdate).diff(dayjs(), 'day'));
+    suffix = diff > 1 ? 'días' : 'día';
+  } else if (diff < 0) {
+    diff = '';
+    suffix = 'unos instantes';
+  }
+
+  await ctx.render('index', { tiempo: `${diff} ${suffix}` });
 });
 
 module.exports = router;
