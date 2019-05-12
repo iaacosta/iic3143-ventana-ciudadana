c053b73ab3df255a37a29f9a951b23a6a183d572
Nacho <iaacosta@uc.cl>
2019-05-12 13:40:02 -0400

    agregada la distinción de minutos

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,16 +4,30 @@ const dayjs = require('dayjs');
 const router = new KoaRouter();
 
 router.get('/', async ctx => {
+  let diff;
+  let suffix;
   const lastUpdate = await ctx.orm.Update.max('createdAt');
-  let diff = Math.abs(dayjs(lastUpdate).diff(dayjs(), 'hour'));
-  let suffix = diff > 1 ? 'horas' : 'hora';
 
-  if (diff > 24) {
-    diff = Math.abs(dayjs(lastUpdate).diff(dayjs(), 'day'));
-    suffix = diff > 1 ? 'días' : 'día';
-  } else if (diff < 0) {
-    diff = '';
-    suffix = 'unos instantes';
+  if (lastUpdate) {
+    const diffFactory = unit => Math.abs(dayjs(lastUpdate).diff(dayjs(), unit));
+    diff = diffFactory('hour');
+    suffix = diff !== 1 ? 'horas' : 'hora';
+
+    if (diff > 24) {
+      diff = diffFactory('day');
+      suffix = diff !== 1 ? 'días' : 'día';
+    } else if (diff < 1) {
+      diff = diffFactory('minute');
+      suffix = diff !== 1 ? 'minutos' : 'minuto';
+    }
+
+    if (diff === 0 && suffix === 'minutos') {
+      diff = '';
+      suffix = 'unos instantes';
+    }
+  } else {
+    diff = '2';
+    suffix = 'dias';
   }
 
   await ctx.render('index', { tiempo: `${diff} ${suffix}` });
