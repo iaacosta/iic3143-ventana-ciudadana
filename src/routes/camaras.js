e66c582df46dc7f8d81964b73e1fee4dd0d95d9e
Nacho <iaacosta@uc.cl>
2019-05-13 14:43:19 -0400

    agregada una tarjeta de senador

--- src/routes/camaras.js
+++ src/routes/camaras.js
@@ -1,29 +1,34 @@
 const KoaRouter = require('koa-router');
-const pkg = require('../../package.json');
-const { Senador, Sequelize, Periodo } = require('../models');
-const Op = Sequelize.Op;
 
 const router = new KoaRouter();
 
 router.get('camaras', '/', async ctx => {
-  const partidos = await Senador.findAll({
-    attributes: ['partido_politico','id',"nombre","apellido_paterno", "apellido_materno"],
+  const partidos = await ctx.orm.Senador.findAll({
+    attributes: ['partido_politico', 'id', 'nombre', 'apellido_paterno', 'url_foto'],
+    include: [
+      {
+        model: ctx.orm.Periodo,
+        attributes: ['final'],
+        where: {
+          final: { [ctx.orm.Sequelize.Op.gte]: 2019 },
+          cargo: 'senador',
+        },
+      },
+    ],
   });
 
-  const years_congress = await Periodo.findAll({
+  const yearsCongress = await ctx.orm.Periodo.findAll({
     attributes: ['sid'],
     where: {
-      final: {[Op.gte]:2019},
-      cargo :"senador"
-    }
+      final: { [ctx.orm.Sequelize.Op.gte]: 2019 },
+      cargo: 'senador',
+    },
   });
 
   await ctx.render('camaras', {
     partidos: JSON.stringify(partidos),
-    years_congress:JSON.stringify(years_congress)
+    yearsCongress: JSON.stringify(yearsCongress),
   });
 });
-
-
 
 module.exports = router;
