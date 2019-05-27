c133c1025c60b6e6a440121bbb52bd0a47f320c9
Esperanza Horn <ehorn@uc.cl>
2019-05-26 21:05:33 -0400

    show - comition: proyectos asociados e info general

---
+++ src/routes/comitions.js
@@ -0,0 +1,72 @@
+const KoaRouter = require('koa-router');
+const dayjs = require('dayjs');
+
+const { paginate } = require('../helpers');
+
+const router = new KoaRouter();
+
+
+router.get('comitions', '/', async ctx => {
+  const coms = await ctx.orm.Comition.findAll();
+
+
+  await ctx.render('comitions/index', {
+    coms,
+  });
+});
+
+
+router.get('comitions', '/show/:id', async ctx => {
+  const comition = await ctx.orm.Comition.findOne({
+    where: {id: ctx.params.id},
+  });
+  const senadores_id = await ctx.orm.SenatorComition.findAll({
+    where: {cid: ctx.params.id},
+  });
+  const proyectos_id = await ctx.orm.ProjectComition.findAll({
+    where: {cid: ctx.params.id},
+  });
+
+  // Ahora a buscar la info particular de senadores y proyectos
+  var senadores = [];
+  const fotos = [];
+
+  for (i = 0; i < senadores_id.lenght; i++) {
+    const sen = await ctx.orm.Senador.findOne({
+      where: {id: senadores_id[i].sid},
+    });
+    if (sen.url_foto == null)
+      fotos.push(
+        'https://d1nhio0ox7pgb.cloudfront.net/_img/o_collection_png/green_dark_grey/256x256/plain/user.png',
+      );
+    else fotos.push(sen.url_foto);
+    senadores.push(sen);
+  }
+
+  console.log(senadores.length);
+
+  var proyectos = [];
+  for (i = 0; i < proyectos_id.length; i++) {
+    const proy = await ctx.orm.Proyecto.findOne({
+      where: {id: proyectos_id[i].pid},
+    });
+    proyectos.push(proy);
+  }
+
+
+  // Ahora calcular porcentajes de partidos
+
+  await ctx.render('comitions/show', {
+    comition,
+    senadores,
+    proyectos,
+  });
+});
+
+
+
+
+
+
+
+module.exports = router;
