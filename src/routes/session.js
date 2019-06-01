bf7de9f52439c0e4161db4f187480f36774f47cf
Nacho <iaacosta@uc.cl>
2019-06-01 14:45:05 -0400

    :lipstick: Added login view and logout function

--- src/routes/session.js
+++ src/routes/session.js
@@ -2,11 +2,16 @@ const KoaRouter = require('koa-router');
 
 const router = new KoaRouter();
 
+router.get('/', async ctx => {
+  if (ctx.session.user) ctx.redirect('/');
+  else await ctx.render('login', { user: null });
+});
+
 router.post('/', async ctx => {
   if (!ctx.session.isNew) ctx.redirect('/');
 
   try {
-    const { username, password } = JSON.parse(ctx.request.body);
+    const { username, password } = ctx.request.body;
     const user = await ctx.orm.User.authenticate(username, password);
     ctx.session.user = user;
     ctx.redirect('/');
