b2a37619d9d4ac341430f8e9138bb7b947b439d7
Nacho <iaacosta@uc.cl>
2019-06-30 18:37:19 -0400

    💄 Added senators followees and modified UI

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,9 +6,12 @@ router.get('user.profile', '/', async ctx => {
   ctx.assert(ctx.session.user, 401);
 
   const { User } = ctx.orm;
-  const user = await User.findAll({ where: { id: ctx.session.user.id } });
+  const user = await User.findOne({ where: { id: ctx.session.user.id } });
+  const followees = await user.getFollowees();
+
   await ctx.render('users/profile', {
-    userObject: user[0].dataValues,
+    userObject: user.dataValues,
+    followees,
     user: ctx.session ? ctx.session.user : null,
   });
 });
